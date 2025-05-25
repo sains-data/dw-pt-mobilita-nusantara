9bf6f6a9a50184f99e0b59f0b0d5aa80b84fa330
Hermawan Manurung <113615395+Hash-sd@users.noreply.github.com>
2025-05-26 00:55:36 +0700

    Update 06_Gold_Layer_Load_Procedure.sql

--- gold/06_Gold_Layer_Load_Procedure.sql
+++ gold/06_Gold_Layer_Load_Procedure.sql
@@ -3,24 +3,42 @@ USE [pt-mobilita-nusantara];
 GO
 
 -- Membuat atau Mengubah Stored Procedure untuk memuat data ke Gold Layer
-CREATE OR ALTER PROCEDURE dbo.LoadGoldLayerData
+CREATE OR ALTER PROCEDURE dbo.LoadGoldLayerData_V2 -- Menggunakan V2 untuk menandai versi revisi
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    PRINT '--- Memulai Proses LoadGoldLayerData_V2 ---';
+
+    DECLARE @SilverCount INT;
+    SELECT @SilverCount = COUNT(*) FROM silver.transformed_car_sales_transactions;
+    PRINT 'Jumlah baris di silver.transformed_car_sales_transactions: ' + CAST(@SilverCount AS VARCHAR(10));
+
+    IF @SilverCount = 0
+    BEGIN
+        PRINT 'PERINGATAN: Tidak ada data di silver.transformed_car_sales_transactions. Proses pemuatan ke gold dihentikan.';
+        RETURN;
+    END
+
     --------------------------------------------------------------------------
     -- 1. Populasi gold.dim_date
     --------------------------------------------------------------------------
+    PRINT 'Memulai populasi gold.dim_date...';
+    -- Jika Anda ingin dim_date di-refresh total setiap kali, uncomment TRUNCATE di bawah.
+    -- Jika tidak, hanya tanggal baru yang akan ditambahkan.
+    -- TRUNCATE TABLE gold.dim_date;
+    -- PRINT 'Tabel gold.dim_date telah di-TRUNCATE (jika diaktifkan).';
+
     INSERT INTO gold.dim_date (date_key, [date], year, month, day, weekday, month_name, quarter)
     SELECT DISTINCT
-        CONVERT(INT, CONVERT(VARCHAR(8), s.Sales_Date, 112)) AS date_key, -- YYYYMMDD format
+        CONVERT(INT, CONVERT(VARCHAR(8), s.Sales_Date, 112)) AS date_key,
         s.Sales_Date,
         YEAR(s.Sales_Date) AS year,
         MONTH(s.Sales_Date) AS month,
         DAY(s.Sales_Date) AS day,
         DATENAME(dw, s.Sales_Date) AS weekday,
-        DATENAME(month, s.Sales_Date) AS month_name, -- Mengisi kolom month_name
-        DATEPART(qq, s.Sales_Date) AS quarter        -- Mengisi kolom quarter
+        DATENAME(month, s.Sales_Date) AS month_name,
+        DATEPART(qq, s.Sales_Date) AS quarter
     FROM
         silver.transformed_car_sales_transactions s
     WHERE
@@ -30,51 +48,46 @@ BEGIN
             WHERE dd.date_key = CONVERT(INT, CONVERT(VARCHAR(8), s.Sales_Date, 112))
         )
         AND s.Sales_Date IS NOT NULL;
-
-    PRINT 'gold.dim_date populated/updated.';
+    PRINT CAST(@@ROWCOUNT AS VARCHAR(10)) + ' baris baru ditambahkan ke gold.dim_date.';
 
     --------------------------------------------------------------------------
     -- 2. Populasi gold.dim_car
     --------------------------------------------------------------------------
+    PRINT 'Memulai populasi gold.dim_car...';
     MERGE gold.dim_car AS Target
     USING (
         SELECT DISTINCT
-            Car_ID,
-            Car_Make,
-            Car_Model,
-            Engine_Type,         -- Kolom dari silver
-            Transmission_Type,   -- Kolom dari silver
-            Car_Color,
-            Body_Style
+            Car_ID, Car_Make, Car_Model, Engine_Type,
+            Transmission_Type, Car_Color, Body_Style
         FROM silver.transformed_car_sales_transactions
         WHERE Car_ID IS NOT NULL
     ) AS Source
     ON Target.car_id = Source.Car_ID
     WHEN MATCHED THEN
         UPDATE SET
-            Target.make = Source.Car_Make,
-            Target.model = Source.Car_Model,
-            Target.engine_type = Source.Engine_Type,               -- Mengisi kolom engine_type
-            Target.transmission_type = Source.Transmission_Type,   -- Mengisi kolom transmission_type
-            Target.color = Source.Car_Color,
-            Target.body_style = Source.Body_Style
+            Target.make = Source.Car_Make, Target.model = Source.Car_Model,
+            Target.engine_type = Source.Engine_Type, Target.transmission_type = Source.Transmission_Type,
+            Target.color = Source.Car_Color, Target.body_style = Source.Body_Style
     WHEN NOT MATCHED BY TARGET THEN
         INSERT (car_id, make, model, engine_type, transmission_type, color, body_style)
         VALUES (Source.Car_ID, Source.Car_Make, Source.Car_Model, Source.Engine_Type, Source.Transmission_Type, Source.Car_Color, Source.Body_Style);
-
-    PRINT 'gold.dim_car populated/updated.';
+    PRINT CAST(@@ROWCOUNT AS VARCHAR(10)) + ' baris terpengaruh (INSERT/UPDATE) di gold.dim_car.';
 
     --------------------------------------------------------------------------
-    -- 3. Populasi gold.dim_dealer
+    -- 3. Populasi gold.dim_dealer (Dengan Perbaikan Duplikasi)
     --------------------------------------------------------------------------
+    PRINT 'Memulai populasi gold.dim_dealer...';
     MERGE gold.dim_dealer AS Target
     USING (
-        SELECT DISTINCT
-            Dealer_Number,
-            Dealer_Name,
-            Dealer_Region
-        FROM silver.transformed_car_sales_transactions
-        WHERE Dealer_Number IS NOT NULL
+        SELECT Dealer_Number, Dealer_Name, Dealer_Region -- Ambil atribut yang relevan
+        FROM (
+            SELECT
+                Dealer_Number, Dealer_Name, Dealer_Region,
+                ROW_NUMBER() OVER(PARTITION BY Dealer_Number ORDER BY Dealer_Name, Dealer_Region) as rn -- Urutkan untuk konsistensi jika ada beberapa nama/region untuk 1 nomor
+            FROM silver.transformed_car_sales_transactions
+            WHERE Dealer_Number IS NOT NULL AND Dealer_Number != '' -- Pastikan dealer number valid
+        ) AS RankedDealers
+        WHERE rn = 1 -- Ambil hanya satu baris unik per Dealer_Number
     ) AS Source
     ON Target.dealer_number = Source.Dealer_Number
     WHEN MATCHED THEN
@@ -84,23 +97,20 @@ BEGIN
     WHEN NOT MATCHED BY TARGET THEN
         INSERT (dealer_number, dealer_name, dealer_region)
         VALUES (Source.Dealer_Number, Source.Dealer_Name, Source.Dealer_Region);
-
-    PRINT 'gold.dim_dealer populated/updated.';
+    PRINT CAST(@@ROWCOUNT AS VARCHAR(10)) + ' baris terpengaruh (INSERT/UPDATE) di gold.dim_dealer.';
 
     --------------------------------------------------------------------------
     -- 4. Populasi gold.dim_customer
     --------------------------------------------------------------------------
+    PRINT 'Memulai populasi gold.dim_customer...';
     MERGE gold.dim_customer AS Target
     USING (
         SELECT DISTINCT
-            Customer_Name,
-            Gender,
-            Annual_Income,
-            Customer_Phone
+            Customer_Name, Gender, Annual_Income, Customer_Phone
         FROM silver.transformed_car_sales_transactions
-        WHERE Customer_Name IS NOT NULL OR Customer_Phone IS NOT NULL
+        WHERE (Customer_Name IS NOT NULL AND Customer_Name != '') OR (Customer_Phone IS NOT NULL AND Customer_Phone != '') -- Minimal ada nama atau telepon
     ) AS Source
-    ON Target.customer_name = Source.Customer_Name AND ISNULL(Target.customer_phone, '') = ISNULL(Source.Customer_Phone, '')
+    ON Target.customer_name = Source.Customer_Name AND ISNULL(Target.customer_phone, 'N/A') = ISNULL(Source.Customer_Phone, 'N/A') -- Mencocokkan berdasarkan nama dan telepon (handle NULL pada telepon)
     WHEN MATCHED THEN
         UPDATE SET
             Target.gender = Source.Gender,
@@ -108,21 +118,17 @@ BEGIN
     WHEN NOT MATCHED BY TARGET THEN
         INSERT (customer_name, gender, annual_income, customer_phone)
         VALUES (Source.Customer_Name, Source.Gender, Source.Annual_Income, Source.Customer_Phone);
-
-    PRINT 'gold.dim_customer populated/updated.';
+    PRINT CAST(@@ROWCOUNT AS VARCHAR(10)) + ' baris terpengaruh (INSERT/UPDATE) di gold.dim_customer.';
 
     --------------------------------------------------------------------------
     -- 5. Populasi gold.fact_car_sales
     --------------------------------------------------------------------------
+    PRINT 'Memulai populasi gold.fact_car_sales...';
     TRUNCATE TABLE gold.fact_car_sales;
+    PRINT 'Tabel gold.fact_car_sales telah di-TRUNCATE.';
 
     INSERT INTO gold.fact_car_sales (
-        car_id_original,
-        date_key,
-        car_key,
-        dealer_key,
-        customer_key,
-        sales_price
+        car_id_original, date_key, car_key, dealer_key, customer_key, sales_price
     )
     SELECT
         s.Car_ID,
@@ -133,23 +139,19 @@ BEGIN
         s.Sales_Price
     FROM
         silver.transformed_car_sales_transactions s
-    LEFT JOIN
+    INNER JOIN -- Menggunakan INNER JOIN untuk memastikan semua kunci ditemukan
         gold.dim_date dd ON dd.date_key = CONVERT(INT, CONVERT(VARCHAR(8), s.Sales_Date, 112))
-    LEFT JOIN
+    INNER JOIN
         gold.dim_car dc ON dc.car_id = s.Car_ID
-    LEFT JOIN
+    INNER JOIN
         gold.dim_dealer ddl ON ddl.dealer_number = s.Dealer_Number
-    LEFT JOIN
-        gold.dim_customer dcu ON dcu.customer_name = s.Customer_Name AND ISNULL(dcu.customer_phone, '') = ISNULL(s.Customer_Phone, '')
+    INNER JOIN
+        gold.dim_customer dcu ON dcu.customer_name = s.Customer_Name AND ISNULL(dcu.customer_phone, 'N/A') = ISNULL(s.Customer_Phone, 'N/A')
     WHERE
-        s.Car_ID IS NOT NULL AND s.Sales_Date IS NOT NULL;
+        s.Sales_Price IS NOT NULL; -- Pastikan ada harga jual untuk fakta
 
-    PRINT 'gold.fact_car_sales populated.';
-    SELECT @@ROWCOUNT AS RowsLoadedIntoFactSales;
+    PRINT CAST(@@ROWCOUNT AS VARCHAR(10)) + ' baris berhasil dimasukkan ke gold.fact_car_sales.';
+    PRINT '--- Proses LoadGoldLayerData_V2 Selesai ---';
 
 END
 GO
-
--- Cara Menjalankan Stored Procedure:
--- EXEC dbo.LoadGoldLayerData;
--- GO
