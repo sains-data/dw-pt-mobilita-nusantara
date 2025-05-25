0a8402b43e839a8548c00f6cd60f929346d7e4d8
Hermawan Manurung <113615395+Hash-sd@users.noreply.github.com>
2025-05-26 00:20:41 +0700

    Update 01_Bronze_Layer_DDL.sql

--- bronze/01_Bronze_Layer_DDL.sql
+++ bronze/01_Bronze_Layer_DDL.sql
@@ -2,37 +2,48 @@
 USE [pt-mobilita-nusantara];
 GO
 
--- Buat skema bronze jika belum ada
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
-BEGIN
-    EXEC('CREATE SCHEMA bronze');
-END
+-- Kosongkan tabel bronze sebelum memuat data baru (opsional, tergantung kebutuhan)
+-- TRUNCATE TABLE bronze.raw_car_sales_transactions;
+-- GO
+
+INSERT INTO bronze.raw_car_sales_transactions (
+    Car_id,
+    [Date],
+    [Customer Name],
+    Gender,
+    [Annual Income],
+    Dealer_Name,
+    Company,
+    Model,
+    Engine,
+    Transmission,
+    Color,
+    [Price ($)],
+    [Dealer_No],
+    [Body Style],
+    Phone,
+    Dealer_Region
+)
+SELECT
+    [Car_id],         -- Sesuaikan jika nama di sumber berbeda
+    [Date],           -- Sesuaikan jika nama di sumber berbeda
+    [Customer Name],  -- Sesuaikan jika nama di sumber berbeda
+    [Gender],         -- Sesuaikan jika nama di sumber berbeda
+    [Annual Income],  -- Sesuaikan jika nama di sumber berbeda
+    [Dealer_Name],    -- Sesuaikan jika nama di sumber berbeda
+    [Company],        -- Sesuaikan jika nama di sumber berbeda
+    [Model],          -- Sesuaikan jika nama di sumber berbeda
+    [Engine],         -- Sesuaikan jika nama di sumber berbeda
+    [Transmission],   -- Sesuaikan jika nama di sumber berbeda
+    [Color],          -- Sesuaikan jika nama di sumber berbeda
+    [Price ($)],      -- Sesuaikan jika nama di sumber berbeda
+    [Dealer_No],    -- Sesuaikan jika nama di sumber berbeda (nama di CSV punya spasi sebelum Dealer_No)
+    [Body Style],     -- Sesuaikan jika nama di sumber berbeda
+    [Phone],          -- Sesuaikan jika nama di sumber berbeda
+    [Dealer_Region]   -- Sesuaikan jika nama di sumber berbeda
+FROM
+    [Car Sales#xlsx - car_data$]; -- Tabel sumber Anda yang sudah diimpor
 GO
 
--- Hapus tabel jika sudah ada
-IF OBJECT_ID('bronze.raw_car_sales_transactions', 'U') IS NOT NULL
-    DROP TABLE bronze.raw_car_sales_transactions;
+SELECT COUNT(*) AS TotalRowsLoaded FROM bronze.raw_car_sales_transactions;
 GO
-
--- Buat tabel baru sesuai dengan struktur data sumber Anda
-CREATE TABLE bronze.raw_car_sales_transactions (
-    Car_id VARCHAR(255),        -- Dari sumber Anda
-    [Date] DATE,                -- Dari sumber Anda (gunakan kurung siku jika nama kolom adalah 'Date')
-    [Customer Name] VARCHAR(255), -- Dari sumber Anda
-    Gender VARCHAR(50),           -- Dari sumber Anda
-    [Annual Income] VARCHAR(255), -- Dari sumber Anda (pertimbangkan DECIMAL jika ini angka)
-    Dealer_Name VARCHAR(255),     -- Dari sumber Anda
-    Company VARCHAR(255),         -- Dari sumber Anda
-    Model VARCHAR(255),           -- Dari sumber Anda
-    Engine VARCHAR(255),          -- Dari sumber Anda
-    Transmission VARCHAR(255),    -- Dari sumber Anda
-    Color VARCHAR(255),           -- Dari sumber Anda
-    [Price ($)] VARCHAR(255),     -- Dari sumber Anda (pertimbangkan DECIMAL jika ini angka)
-    [Dealer_No] VARCHAR(255),   -- Dari sumber Anda
-    [Body Style] VARCHAR(255),    -- Dari sumber Anda
-    Phone VARCHAR(50),            -- Dari sumber Anda
-    Dealer_Region VARCHAR(255)    -- Dari sumber Anda
-);
-GO
-
-SELECT 'Tabel bronze.raw_car_sales_transactions berhasil dibuat dengan struktur baru.' AS Status;
