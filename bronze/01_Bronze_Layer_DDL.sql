18d1cc7cdec698c6150773282ea21e376086a800
zranmirzan <161723498+zranmirzan@users.noreply.github.com>
2025-05-27 19:50:30 +0700

    Update 01_Bronze_Layer_DDL.sql

--- bronze/01_Bronze_Layer_DDL.sql
+++ bronze/01_Bronze_Layer_DDL.sql
@@ -1,44 +1,35 @@
+-- File: 01_Bronze_Layer_DDL.sql (Versi Disesuaikan)
 USE [pt-mobilita-nusantara];
 GO
 
-INSERT INTO bronze.raw_car_sales_transactions (
-    Car_id,
-    [Date],
-    [Customer Name],
-    Gender,
-    [Annual Income],
-    Dealer_Name,
-    Company,
-    Model,
-    Engine,
-    Transmission,
-    Color,
-    [Price ($)],
-    [Dealer_No],
-    [Body Style],
-    Phone,
-    Dealer_Region
-)
-SELECT
-    [Car_id],        
-    [Date],           
-    [Customer Name],  
-    [Gender],         
-    [Annual Income],  
-    [Dealer_Name],    
-    [Company],        
-    [Model],          
-    [Engine],         
-    [Transmission],   
-    [Color],          
-    [Price ($)],      
-    [Dealer_No],    
-    [Body Style],     
-    [Phone],          
-    [Dealer_Region]   
-FROM
-    [Car Sales#xlsx - car_data$]; 
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
+BEGIN
+   EXEC('CREATE SCHEMA bronze');
+END
 GO
 
-SELECT COUNT(*) AS TotalRowsLoaded FROM bronze.raw_car_sales_transactions;
+IF OBJECT_ID('bronze.raw_car_sales_transactions', 'U') IS NOT NULL
+   DROP TABLE bronze.raw_car_sales_transactions;
 GO
+
+CREATE TABLE bronze.raw_car_sales_transactions (
+   Car_id VARCHAR(255),
+   [Date] DATE,
+   [Customer Name] VARCHAR(255),
+   Gender VARCHAR(50),
+   [Annual Income] VARCHAR(255),
+   Dealer_Name VARCHAR(255),
+   Company VARCHAR(255),
+   Model VARCHAR(255),
+   Engine VARCHAR(255),
+   Transmission VARCHAR(255),
+   Color VARCHAR(255),
+   [Price ($)] VARCHAR(255),
+   [Dealer_No] VARCHAR(255),
+   [Body Style] VARCHAR(255),
+   Phone VARCHAR(50),
+   Dealer_Region VARCHAR(255)
+);
+GO
+
+PRINT 'Tabel bronze.raw_car_sales_transactions berhasil dibuat/diperbarui.';
